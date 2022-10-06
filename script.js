e41769a0a72b119695ab263b5b1ff3ecec77c376
Fachrul <57552713+fachrul990@users.noreply.github.com>
2022-10-06 17:19:55 +0700

    tambah fungsi temukan()

--- script.js
+++ script.js
@@ -9,5 +9,53 @@ const anggota = [
 document.getElementById("textkelompok").innerHTML = anggota.map(getKelompok);
 
 //Fungsi Temukan
-fuction.temukan{
+window.temukan =  () => {
+  const filteredData = input.value == "" ? alert("Input Tidak Boleh Kosong!") : filterData(input.value)
+  result.innerHTML = ''
+  !filteredData.length ? result.innerHTML = '<p>Data Tidak Ditemukan</p>' :
+    filteredData.forEach((el) => {
+      result.innerHTML += `<h1>${el.nama_lengkap}</h1>
+                <table>
+                    <tr>
+                        <td>Nama Lengkap</td>
+                        <td>:</td>
+                        <td>${el.nama_lengkap}</td>
+                    </tr>
+                    <tr>
+                        <td>Nama Panggilan</td>
+                        <td>:</td>
+                        <td>${el.nama_panggilan}</td>
+                    </tr>
+                    <tr>
+                        <td>NIM</td>
+                        <td>:</td>
+                        <td>${el.nim}</td>
+                    </tr>
+                    <tr>
+                        <td>Nomor Telepon</td>
+                        <td>:</td>
+                        <td>${el.nomor_telepon}</td>
+                    </tr>
+                    <tr>
+                        <td>ID Line</td>
+                        <td>:</td>
+                        <td>${el.id_line}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>:</td>
+                        <td>${el.email}</td>
+                    </tr>
+                    <tr>
+                        <td>Hobi</td>
+                        <td>:</td>
+                        <td>${el.hobi}</td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Lahir</td>
+                        <td>:</td>
+                        <td>${el.tanggal_lahir}</td>
+                    </tr>
+                </table>`
+    })
 }
