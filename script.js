2c01dc9ffb85b62cf4857d5374916d833285a9a2
ErlinSofia <64193144+ErlinSofi18@users.noreply.github.com>
2022-10-06 17:09:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//Menampilkan Nama Kelompok Menggunakan Function Map (Pengolahan Array)
+const anggota = [
+  { nama: "Erlin Sofia" },
+  { nama: "Fauzan Zaman" },
+  { nama: "Fachrul" },
+  { nama: "Muhamad Ikhsan" },
+];
+
+document.getElementById("textkelompok").innerHTML = anggota.map(getKelompok);
+
+//Fungsi Temukan
+fuction.temukan{
+}
